cd1cf90819bab3857d3872a597abecab109440bf
hansludvig <hans.ludvig.kleivdal@outlook.com>
2018-04-06 17:29:38 +0200

    mutation: work in progress, need to add resolver

--- graphqlapi/mutation.go
+++ graphqlapi/mutation.go
@@ -1,7 +1,13 @@
 package graphqlapi
 
-//Mutation - GraphQL mutation
 /*
+import (
+	"github.com/autograde/aguis/graphqlapi/objects"
+	gql "github.com/graphql-go/graphql"
+)
+
+//Mutation - GraphQL mutation
+
 func Mutation(db database.Database) *gql.Object {
 	return gql.NewObject(gql.ObjectConfig{
 		Name: "Mutation",
@@ -9,8 +15,11 @@ func Mutation(db database.Database) *gql.Object {
 			"newCourse": &gql.Field{
 				Type: objects.CourseType,
 				Args: gql.ArgumentConfig{
-					Type: // Input object goes here
-				}
+					"usr": &gql.ArgumentConfig{
+						Type: gql.NewNonNull(objects.CourseType) // Input object goes here
+					},
+				},
+				Resolve:
 			},
 		},
 	})
